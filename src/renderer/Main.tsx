9a2242aa6e6fefbedda883c04fa937f1f6d07d69
KutenkovV <kutenkovvlad017@gmail.com>
2022-11-15 22:19:24 +0800

    +

--- src/renderer/Main.tsx
+++ src/renderer/Main.tsx
@@ -7,12 +7,82 @@ const Main: React.FC = () => {
   console.log(data_chart);
 
   var road_2 = {
-    flatness_road_lane_1: 5,
+    flatness_road_lane_1: 2.2,
     road_defects_1: 4.5,
     road_grip_1: 4.3,
     current_year: 5,
     future_year: 3,
+
+    road_class: '1A',
+    road_type: false,
   };
+
+  // Типы наверное просто в тупую перечислю
+  // типа: "1А, 1Б, 1В, 2, 3 и т.д"
+
+  iri_score(road_2);
+
+  function iri_score(param: any) {
+    let IRI = 4.5;
+    let r_class = '3';
+    let r_type = true; // true - Капитальный false - Облегченный
+
+    let iri_score;
+
+    // Оцениваю без округлений, строго по табличным значениям
+    // КАПИТАЛЬНЫЙ
+    if (r_type === true) {
+      if (r_class === '1A' || r_class === '1Б') {
+        if (IRI <= 2.6) iri_score = 5;
+        else if (IRI <= 3.1) iri_score = 4;
+        else if (IRI <= 3.7) iri_score = 3;
+        else iri_score = 2;
+      }
+
+      if (r_class === '1B' || r_class === '2') {
+        if (IRI <= 3.1) iri_score = 5;
+        else if (IRI <= 3.6) iri_score = 4;
+        else if (IRI <= 4.2) iri_score = 3;
+        else iri_score = 2;
+      }
+
+      if (r_class === '3') {
+        if (IRI <= 3.3) iri_score = 5;
+        else if (IRI <= 3.8) iri_score = 4;
+        else if (IRI <= 4.5) iri_score = 3;
+        else iri_score = 2;
+      }
+
+      if (r_class === '4') {
+        if (IRI <= 5) iri_score = 3;
+        else iri_score = 2;
+      }
+      // ОБЛЕГЧЕННЫЙ
+    } else {
+      if (r_class === '3') {
+        if (IRI <= 3.5) iri_score = 5;
+        else if (IRI <= 4.5) iri_score = 4;
+        else if (IRI <= 5.5) iri_score = 3;
+        else iri_score = 2;
+      }
+
+      if (r_class === '4') {
+        if (IRI <= 2.5) iri_score = 5;
+        else if (IRI <= 4.5) iri_score = 4;
+        else if (IRI <= 6.5) iri_score = 3;
+        else iri_score = 2;
+      }
+
+      if (r_class === '5') {
+        if (IRI <= 3.5) iri_score = 5;
+        else if (IRI <= 5.5) iri_score = 4;
+        else if (IRI <= 7.5) iri_score = 3;
+        else iri_score = 2;
+      }
+    }
+
+    console.log(iri_score);
+  }
 
   function road_degradation(param: any) {
     let IRI = road_2.flatness_road_lane_1;
@@ -50,9 +120,6 @@ const Main: React.FC = () => {
         },
       ];
 
-      console.log(array1);
-      
-
       array2 = [
         {
           name: 'IRI',
@@ -82,6 +149,8 @@ const Main: React.FC = () => {
           value: (C **= e3),
         },
       ];
+
+      console.log(array2);
     }
 
     let data = {
@@ -213,23 +282,23 @@ const Main: React.FC = () => {
       <div className="card-form">
         <div className="form-input">
           <label>Введите оценку IRI (Ровность)</label>
-          <input placeholder='Оценка IRI' type="number" />
+          <input placeholder="Оценка IRI" type="number" />
         </div>
         <div className="form-input">
           <label>Введите оценку J (Дефектность)</label>
-          <input placeholder='Оценка J' type="number" />
+          <input placeholder="Оценка J" type="number" />
         </div>
         <div className="form-input">
           <label>Введите оценку С (Сцепление)</label>
-          <input placeholder='Оценка C' type="number" />
+          <input placeholder="Оценка C" type="number" />
         </div>
         <div className="form-input">
           <label>Введите текущий период эксплуатации</label>
-          <input placeholder='Год' type="number" />
+          <input placeholder="Год" type="number" />
         </div>
         <div className="form-input">
           <label>Введите на сколько лет делать прогноз</label>
-          <input placeholder='Год' type="number" />
+          <input placeholder="Год" type="number" />
         </div>
 
         {/* Кнопочка */}
