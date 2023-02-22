55f1d0d7eaa9b0140eb30d5be1f6dddfe70a4dc1
KutenkovV <kutenkovvlad017@gmail.com>
2023-02-22 10:46:45 +0800

    +

--- src/renderer/page/Main.tsx
+++ src/renderer/page/Main.tsx
@@ -20,18 +20,34 @@ function Main() {
   return (
     <>
       <div className="form__input">
-        <div className="form-view">{items.map((item) => (
-          <div id={roadStatus(item.value.IRI)}>{item.value.IRI}</div>
-        ))}
+        <div className="form-marker">
+          <div className="marker-item">
+            <div id="rectangle_best"></div>
+            <p>Ремонт не требуется</p>
+          </div>
+          <div className="marker-item">
+            <div id="rectangle_good" />
+            <p>Требуется ремонт</p>
+          </div>
+          <div className="marker-item">
+            <div id="rectangle_bad" />
+            <p>Требуется Капитальный ремонт</p>
+          </div>
+        </div>
+        <div className="form-view">
+          {items.map((item) => (
+            <div id={roadStatus(item.value.IRI)}>{item.value.IRI}</div>
+          ))}
         </div>
         <div>
-          <button
+          <label
+            className="lbl-button"
             onClick={() => {
               navigate('/');
             }}
           >
             Изменить данные
-          </button>
+          </label>
           <button onClick={Submit}>Получить данные</button>
         </div>
       </div>
@@ -41,11 +57,11 @@ function Main() {
 
 function roadStatus(IRI: string) {
   if (IRI === 5) {
-    return "rectangle_best"
+    return 'rectangle_best';
   }
   if (IRI === 4) {
-    return "rectangle_good"
-  } else return "rectangle_bad"
+    return 'rectangle_good';
+  } else return 'rectangle_bad';
 }
 
 export default Main;
