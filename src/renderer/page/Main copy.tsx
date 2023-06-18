20ed0a444e6361994943326795ba17fddc7e3288
KutenkovV <kutenkovvlad017@gmail.com>
2023-06-18 18:07:20 +0800

    треш

--- src/renderer/page/Main copy.tsx
+++ src/renderer/page/Main copy.tsx
@@ -8,17 +8,17 @@ import "../../styles/accordeon.scss"
 import "./Main.scss"
 
 /// Для тултипов (https://react-tooltip.com/docs/getting-started)
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
+// import 'react-tooltip/dist/react-tooltip.css'
+// import { Tooltip } from 'react-tooltip'
 /// Для аккордеона (https://szhsin.github.io/react-accordion/docs/getting-started)
 import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
 import chevronDown from "../chevron-down.svg";
 import { dataMain, set_dataRemont, set_remontList } from 'renderer/store/MainStore';
 import { data } from 'renderer/store/UploadStore';
 import StudentMenu from 'renderer/components/StudentMenu/StudentMenu';
+import { ClickAwayListener, Tooltip } from '@mui/material';
 
 function Main(this: any) {
-  var ReactDOMServer = require('react-dom/server');
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [index, setIndex] = useState(0)
@@ -95,9 +95,15 @@ function Main(this: any) {
     return remont_final;
   }
 
+  const [open, setOpen] = useState(false)
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
 
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
 
-  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <div className="form__input">
@@ -117,50 +123,52 @@ function Main(this: any) {
         </div>
         <div className="form-view">
           {datares.map((item: any, index: number) => (
-            <div onMouseEnter={() => setIsOpen(true)} onClick={() => (setIndex(index), setIsOpen(!isOpen))} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-              data-tooltip-id='my-tooltip'
-              data-avg={item.AVG}
-              data-avg-color={roadStatus(item.AVG)}
-              data-iri={item.IRI.toFixed(2)}
-              data-j={item.J.toFixed(2)}
-              data-c={item.C.toFixed(2)}
-              data-recommendation={item.recommendation}
-              data-some-relevant-attr={index + 1}
-              id={roadStatus(item.AVG)} key={index}>
-              <a>{item.AVG}</a>
-            </div>
+            <ClickAwayListener onClickAway={handleTooltipClose}>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="Add" onClick={handleTooltipOpen}>
+                <Tooltip arrow title={[
+                  <div style={{ width: '13rem' }} className='tooltip-form'>
+                    <p>{'Участок номер № - ' + (index + 1)}</p>
+                    <p className='tooltip-avg-status'>
+                      {'Состояние(среднее): ' + item.AVG}
+                      <label style={{ marginLeft: '0.45rem', width: '12px', height: '12spx' }} id={'rectangle_bad'}>s</label>
+                    </p>
+                    <p>{'Оценка IRI: ' + item.IRI.toFixed(2)}</p>
+                    <p>{'Оценка J: ' + item.J.toFixed(2)}</p>
+                    <p>{'Оценка C: ' + item.C.toFixed(2)}</p>
+                  </div>,
+                  <hr />,
+                  <p style={{ fontSize: '13px'}}>{item.recommendation}</p>
+                ]}>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                    id={roadStatus(item.AVG)} key={index}>
+                    <a>{item.AVG}</a>
+                  </div>
+                </Tooltip>
+              </Tooltip>
+            </ClickAwayListener>
           ))}
-
-          <Tooltip style={{ width: '18rem' }} className='tooltip-form' classNameArrow="tooltip-arrow"
-            id="my-tooltip"
-            render={({ activeAnchor }) => (
-              <div>
-                <p>Участок номер № - {activeAnchor?.getAttribute('data-some-relevant-attr') || 'not set'}</p>
-                <p className='tooltip-avg-status'>Состояние (среднее): {activeAnchor?.getAttribute('data-avg') || 'not set'}
-                  <label style={{ marginLeft: '0.45rem', width: '12px', height: '12px' }} id={activeAnchor?.getAttribute('data-avg-color') || 'not set'} />
-                </p>
-                <p>IRI (Ровность): {activeAnchor?.getAttribute('data-iri') || 'not set'}</p>
-                <p>J (Дефектность): {activeAnchor?.getAttribute('data-j') || 'not set'}</p>
-                <p>C (Сцепление): {activeAnchor?.getAttribute('data-c') || 'not set'}</p>
-                <hr style={{ margin: '0' }} />
-                <label style={{ color: 'red', fontSize: '13px' }}>Рекомендуется</label>
-                <p style={{ display: 'flex', fontSize: '12px' }}>{activeAnchor?.getAttribute('data-recommendation')?.split(',').join('') || 'Рекомендаций нет'}</p>
-              </div>
-            )}
-          />
-
         </div>
 
         {/* <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M9.50043 1.5C9.50043 2.32812 8.82855 3 8.00043 3C7.1723 3 6.50043 2.32812 6.50043 1.5C6.50043 0.671875 7.1723 0 8.00043 0C8.82855 0 9.50043 0.671875 9.50043 1.5ZM0.306677 6.2125C0.466052 5.83125 0.903552 5.65 1.28793 5.80937L1.8973 6.0625L2.61293 4.87187C3.1223 4.01875 4.04105 3.5 5.03168 3.5C6.63793 3.5 8.06293 4.52812 8.5723 6.05312L9.65355 9.29375L12.1317 10.3281L13.2004 8.90312C13.4004 8.6375 13.7192 8.4875 14.0504 8.50313C14.3817 8.51875 14.6848 8.70312 14.8567 8.9875L17.8567 13.9875C18.041 14.2969 18.0473 14.6812 17.8692 14.9937C17.691 15.3062 17.3598 15.5 17.0004 15.5H9.00043C8.65355 15.5 8.33168 15.3219 8.15043 15.025C7.96918 14.7281 7.95043 14.3625 8.10668 14.0531L8.60668 13.0531C8.77543 12.7156 9.1223 12.5 9.50043 12.5H10.5004L11.2036 11.5625L0.712927 7.19375C0.331677 7.03437 0.150427 6.59688 0.309802 6.2125H0.306677ZM2.89418 9.08125L6.39418 10.5813C6.76293 10.7375 7.00043 11.1 7.00043 11.5V14.5C7.00043 15.0531 6.55355 15.5 6.00043 15.5C5.4473 15.5 5.00043 15.0531 5.00043 14.5V12.1594L3.10668 11.3469L1.95043 14.8156C1.77543 15.3406 1.2098 15.6219 0.684802 15.4469C0.159802 15.2719 -0.121448 14.7063 0.0504268 14.1844L1.55043 9.68437L1.89418 8.65312L2.89418 9.08125Z" fill="#646464" />
         </svg> */}
         <div className="form-switch">
-          <div className="lbl-button" onClick={() => { navigate('/') }}>
-            <svg style={{ paddingRight: '0.5em', cursor: 'pointer' }} width="13" height="13" viewBox="0 0 11 13" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.79242 0.952347L6.75257 2.08672L9.54554 5.1336L10.5854 3.99922C11.1225 3.41328 11.1225 2.46407 10.5854 1.87813L9.7389 0.952347C9.20179 0.366409 8.33167 0.366409 7.79456 0.952347H7.79242ZM6.26703 2.61641L1.25902 8.08204C1.03558 8.32579 0.8723 8.62813 0.782065 8.9586L0.0215186 11.7781C-0.0321924 11.9773 0.0172217 12.1906 0.150425 12.3359C0.283628 12.4813 0.479136 12.5352 0.659604 12.4789L3.24417 11.6492C3.5471 11.5508 3.82425 11.3727 4.04769 11.1289L9.05999 5.66328L6.26703 2.61641Z" />
-            </svg>
-            <a style={{ fontSize: '16px' }}>Изменить данные</a>
-          </div>
+          <label
+            className="lbl-button"
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Изменить данные
+          </label>
           <StepProgressBar />
         </div>
       </div >
@@ -204,18 +212,6 @@ function roadStatus(IRI: number) {
 function indexCheck(index: number) {
   if (index === 0) return 'текущий'
   else return index;
-}
-
-function rem_gen(item: any) {
-  let a = (
-    <>
-      <div id="my-div">
-        <h4>This is a heading tag.</h4>
-        <p>This is a paragraph.</p>
-      </div>
-      <div id="output"></div>
-    </>
-  )
 }
 
 export default Main;
